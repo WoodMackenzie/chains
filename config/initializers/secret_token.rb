88dbc251e5e6781c9e493376901bd47aa1e75a70
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-11 13:14:52 -0600

    Initial build of rails project named 'Chains'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chains::Application.config.secret_key_base = 'a0287699539445461c827c4624f6d295e2546718d9c1edfb0ed7794f11f4f39f7b609ea2e216424b1943176d14637ccfa9fa4810c2be6f3a0994faf0e09dda09'
