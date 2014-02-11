c93312e0601923b4cf70441ed81c6635e3928a2f
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-11 15:57:40 -0600

    Added in Twitter Bootstrap, Devise, and Simple Form into the code base, along with tweaks to the code requested by Devise.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,9 @@ Chains::Application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Added after Devise installation, defining a default url option
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 end
