ff9bb98ea355eabedda3429fd2619f7e48ebe91f
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-12 10:04:37 -0600

    Added dropdown menu for devise in the navbar, added functionality for users to add in first and last names, and added in those selections to the views for new users and users who want to edit information.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211220154) do
+ActiveRecord::Schema.define(version: 20140212153357) do
 
   create_table "processing_units", force: true do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20140211220154) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
