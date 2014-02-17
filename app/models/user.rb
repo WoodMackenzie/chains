1a61c8ac235d1ac4d2ea56bab253d346d3d2a18d
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-17 12:37:47 -0600

    Added in tests to ensure users put in first name, last name, and e-mail address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
   def full_name
     first_name + " " + last_name
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "A user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  test "A user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  test "A user should enter an e-mail address" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:email].empty?
+  end
+
 end
