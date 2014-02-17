a6e38cdaba55c3dc164a386e8e435235bbee3ca1
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-17 15:40:42 -0600

    Added in 'shoulda' gem for testing common rails functionality and added in tests for the new user page.  These tests included presence of first name, last name, and email, as well as uniqueness of email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :first_name, :last_name
+
   def full_name
     first_name + " " + last_name
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  should validate_presence_of(:email)
+  should validate_uniqueness_of(:email)
 end
