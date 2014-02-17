44d40604b4ec291c2bd83de0776c01e302715391
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-17 09:32:03 -0600

    Initial pass at working in names into the template, as well as fixing an issue with the link in the navbar for 'Chains'.

--- app/models/processing_unit.rb
+++ app/models/processing_unit.rb
@@ -4,5 +4,10 @@ class ProcessingUnit < ActiveRecord::Base
   after_validation :reverse_geocode
   has_many :names, as: :nameable
   has_many :comments, as: :commentable
-  
+
+  def name_at_date(which_date)
+    ordered_names = ProcessingUnit.names.order(active_date: :desc)
+    active_names = ordered_names.where(names.active_date < which_date)
+    
+  end
 end
