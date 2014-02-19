a68d0b734e92770620c6b1c9441651d1ebeb0db2
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-02-19 09:54:49 -0600

    Added in functions to sort and filter date stamps on names in processing units.

--- app/models/processing_unit.rb
+++ app/models/processing_unit.rb
@@ -10,4 +10,17 @@ class ProcessingUnit < ActiveRecord::Base
     active_names = ordered_names.where(names.active_date < which_date)
     
   end
+
+  def name_history
+    self.names.order(active_date: :desc).order(updated_at: :desc)
+  end
+
+  def last_updated_name_history
+    self.name_history.group(:active_date)
+  end
+
+  def historical_names(view_date)
+    self.last_updated_name_history.where("active_date<=?",view_date)
+  end
+
 end
