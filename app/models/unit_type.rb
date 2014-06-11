0b34b5bb237d85225572100d1157b8cb787b2c4f
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-06-10 21:40:40 -0500

    Finished work on the unit types section, added capability to chain unit types together and the views to be able to navigate through them.

--- app/models/unit_type.rb
+++ app/models/unit_type.rb
@@ -1,6 +1,17 @@
 class UnitType < ActiveRecord::Base
 
-  def parent_variable
+  def self.root_unit_type_ids
+    array = Array.new
+    UnitType.all.each do |unit_type|
+      if unit_type.parent_id.nil?
+        array.push(unit_type.id)
+      end
+    end
+    return array
+  end
+
+  def alpha_unit_types
+    self.order(description: :asc)
   end
 
 end
