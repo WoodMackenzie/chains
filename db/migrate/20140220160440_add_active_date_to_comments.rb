cf2a88ed47d811c1e20bd5155cd46a1d2a522b4c
ryanmacaluso <ryan.macaluso@woodmac.com>
2014-06-21 11:53:27 -0500

    Update 20140220160440_add_active_date_to_comments.rb
    Removed re-casting of 'active_date' variable in 'role_assignments' table, as this has been taken care of in the previous migration.

--- db/migrate/20140220160440_add_active_date_to_comments.rb
+++ db/migrate/20140220160440_add_active_date_to_comments.rb
@@ -1,6 +1,5 @@
 class AddActiveDateToComments < ActiveRecord::Migration
   def change
     add_column :comments, :active_date, :date
-    change_column :role_assignments, :active_date, :date
   end
 end
