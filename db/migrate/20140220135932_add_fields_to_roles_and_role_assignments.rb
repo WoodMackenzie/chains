dc10afd15b55b0a85d29f0da11b3d739f1354352
ryanmacaluso <ryan.macaluso@woodmac.com>
2014-06-21 11:52:32 -0500

    Update 20140220135932_add_fields_to_roles_and_role_assignments.rb
    Changed the 'active_date' migration to be of type 'date' rather than type 'integer'.

--- db/migrate/20140220135932_add_fields_to_roles_and_role_assignments.rb
+++ db/migrate/20140220135932_add_fields_to_roles_and_role_assignments.rb
@@ -7,7 +7,7 @@ class AddFieldsToRolesAndRoleAssignments < ActiveRecord::Migration
     add_column :role_assignments, :holder_type, :string
     add_column :role_assignments, :subject_id, :integer
     add_column :role_assignments, :subject_type, :string
-    add_column :role_assignments, :active_date, :integer
+    add_column :role_assignments, :active_date, :date
     add_column :role_assignments, :share, :float
     add_column :role_assignments, :user_id, :integer
   end
