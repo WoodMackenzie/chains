6b8dc293d785ad6ae62e6aa3f947b4b9c384b80a
Ryan Macaluso <ryan.macaluso@woodmac.com>
2014-06-27 09:27:34 -0500

    Biggest change was to the search functionality, adding in searches into the unit type, location, commodity throughputs, and operators instead of just the name.  Also changed the form views in organizations and processing units to keep the 'Edit' and 'Back' buttons inside the form's well.

--- app/models/processing_unit.rb
+++ app/models/processing_unit.rb
@@ -59,20 +59,78 @@ class ProcessingUnit < ActiveRecord::Base
 
   def self.search(search)
     if search
-      array = Array.new
+      id_array = Array.new
+      unit_array = Array.new
+      unit_type_array = Array.new
       search_condition = "%" + search + "%"
-      name_results = Name.where('name LIKE ? and nameable_type = ?', search_condition, "ProcessingUnit")
-      name_results.all.each do |name_result|
-        array.push(name_result.nameable)
-      end        
-      # name_results.all.each do |name_result|
-      #   # if name_result.nameable_type = "ProcessingUnit"
-      #   #   array.push(name_result.nameable)
-      #   # elsif name_result.nameable_type = "Organization"
-      #   #   array.push(name_result.nameable.role_holder_assignments.where('subject_type = ?', "ProcessingUnit").subjects)
-      #   # end
-      # end
-      return array
+
+      # Identify all of the processing unit names that have the search term within them
+      name_results_1 = Name.where('name LIKE ? and nameable_type = ?', search_condition, "ProcessingUnit")
+      name_results_1.all.each do |name_result|
+        id_array.push(name_result.nameable_id)
+      end
+
+      # Identify all of the organizations that have the search term within their names
+      organization_search = Name.where('name LIKE ? and nameable_type = ?', search_condition, "Organization")
+      organization_search.all.each do |organization_action|
+        name_results_2 = RoleAssignment.where('holder_type = ? AND subject_type = ? AND holder_id = ?', "Organization", "ProcessingUnit", organization_action.nameable_id)
+        name_results_2.all.each do |name_result|
+          if id_array.include?(name_result.subject_id)
+          else
+            id_array.push(name_result.subject_id)
+          end
+        end
+      end
+
+      # Identify any throughputs that have the search term within their description
+      Commodity.all.each do |this_commodity|
+        if this_commodity.description.downcase.include?(search.downcase)
+          Throughput.all.each do |this_throughput|
+            if this_throughput.commodity_id == this_commodity.id
+              if id_array.include?(this_throughput.processing_unit_id)
+              else
+                id_array.push(this_throughput.processing_unit_id)
+              end
+            else
+            end
+          end
+        else
+        end
+      end
+
+      # Identify any unit types that have the search term within their aggregated description
+      all_unit_types = UnitType.all.unit_type_list
+      all_unit_types.each do |this_unit_type|
+        if this_unit_type.at(1).downcase.include?(search.downcase)
+          this_unit_type_id = this_unit_type.at(0)
+          processing_unit_results = ProcessingUnit.where('unit_type_id = ?', this_unit_type_id)
+          processing_unit_results.all.each do |this_processing_unit|
+            if id_array.include?(this_processing_unit.id)
+            else
+              id_array.push(this_processing_unit.id)
+            end
+          end
+        else
+        end
+      end
+
+      # Identify any processing units that have city / state / country names that contain the search term
+      processing_unit_results = ProcessingUnit.where('city LIKE ? OR state LIKE ? OR country LIKE ?', search_condition, search_condition, search_condition)
+      processing_unit_results.each do |this_processing_unit|
+        if id_array.include?(this_processing_unit.id)
+        else
+          id_array.push(this_processing_unit.id)
+        end
+      end
+
+      # Compile that list of processing unit IDs into a list of processing unit objects
+      id_array.each do |id_array_action|
+        name_results_final = Name.where('nameable_id = ? AND nameable_type = ?', id_array_action, "ProcessingUnit").last.nameable
+        unit_array.push(name_results_final)
+      end
+
+      # Return those objects
+      return unit_array
     else
       ProcessingUnit.all
     end
